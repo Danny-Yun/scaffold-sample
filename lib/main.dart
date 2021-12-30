a48bfb149fb2fecd2eaff30057687d5e8d865303
Riudiu <lacvert13@gmail.com>
2021-12-30 19:32:31 +0900

    Drawer 메뉴 생성

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,25 @@ class _ScaffoldSampleState extends State<ScaffoldSample> {
       body: Center(
         child: Text('Contents'),
       ),
+
+      // Drawer 메뉴 생성
+      drawer: Drawer(
+        child: Center(
+          child: Text('슬라이드 메뉴'),
+        ),
+      ),
+      // 버튼을 누르지 않아도 슬라이드로 메뉴를 가지고 올 수 있는 영역을 지정
+      drawerEdgeDragWidth: 100,
+      // 위 기능을 사용할 수 없게 홀드
+      drawerEnableOpenDragGesture: false,
+      // endDrawerEnableOpenDragGesture: false,
+      // 앞에 end가 추가되면 오른쪽에 위치시킬 수 있음
+      endDrawer: Drawer(
+        child: Center(
+          child: Text('슬라이드 메뉴'),
+        ),
+      ),
+
       // 아래 쪽에 내비게이션바 생성
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
